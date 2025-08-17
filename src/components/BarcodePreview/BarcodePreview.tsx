6e2f628eb2d2b1275469ca27f2bc7fd7e25e4ec2
Joey Wasin <wasin.ww1402@gmail.com>
2025-08-17 19:12:00 +0700

    feat: fixed width of barcode preview

--- src/components/BarcodePreview/BarcodePreview.tsx
+++ src/components/BarcodePreview/BarcodePreview.tsx
@@ -12,9 +12,11 @@ export default function BarcodePreview({ value }: { value?: string }) {
     <div className='flex justify-center items-center min-h-[300px] md:min-h-[500px] w-full bg-neutral-100 px-4 md:px-6'>
       <Barcode
         id='barcode-preview'
+        width={2}
         value={value}
         renderer={'canvas'}
         background='white'
+        className={'w-full sm:w-auto'}
       />
     </div>
   );
